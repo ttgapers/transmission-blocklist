9489b5afbb201d0879a6c7ec4a347b4a9b80a93b
Sean Hsu <sahsu.mobi@gmail.com>
2020-07-02 12:07:54 +0800

    Update blocklist-build.sh

--- blocklist-build.sh
+++ blocklist-build.sh
@@ -1,2 +1,8 @@
 #!/bin/sh
-curl -s https://www.iblocklist.com/lists.php | sed -n "s/.*value='\(http.*\)'.*/\1/p" | sed "s/\&amp;/\&/g" | sed "s/http/\"http/g" | sed "s/gz/gz\"/g" | xargs wget -O - |  gunzip | egrep -v '^#' | gzip > blocklist.gz
+curl -s https://www.iblocklist.com/lists.json | \
+    jq -r '.lists[] | .name as $n | select((.subscription == "false") and ($n | startswith("iana-") | not) and (["fornonlancomputers", "bogon", "The Onion Router"] | index($n) | not)) | .list' | \
+    awk 'length($0) > 2 { print "http://list.iblocklist.com/?fileformat=p2p&archiveformat=gz&list=" $0 }' | \
+    xargs wget -O - | \
+    gunzip | \
+    egrep -v '^#' | \
+    gzip > blocklist.gz
